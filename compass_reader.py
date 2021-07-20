fb805f9ec8b9e1f81617385a376782f5f59b99cb
tilimieskotilainen <77461141+tilimieskotilainen@users.noreply.github.com>
2021-07-20 21:48:38 +0300

    Update compass_reader.py

--- compass_reader.py
+++ compass_reader.py
@@ -6,13 +6,13 @@ import json
 hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
 
 PI = 3.14159265
-eranto = 9
+eranto = 9 #magnetic declination in Espoo (called eranto in Finnish)
 
-of = open("compass_offsets.txt") #Open file that contains offsets
-offsets = json.loads(of.read())
-of.close()
+of = open("compass_offsets.txt") #Open file that contains offsets, saved by the calibration utility
+offsets = json.loads(of.read()) #Read the JSON data from the file and store as dictionary called "offsets"
+of.close() #Close file
 
-heading = 0
+heading = 0 #Initiate variable for heading
 
 def read_compass():
 
@@ -20,21 +20,26 @@ def read_compass():
     
     hmc5883l.setContinuousMode()
     
-    global heading
+    global heading #declare global variable to be able to change it's value within this function
 
+    #Start loop to read compass again and again
     while True:
-        (x, y, z) = hmc5883l.getAxes()
+        (x, y, z) = hmc5883l.getAxes() #Read values from compass and save as variables x, y and z
         
-        x = x - offsets["x_offset"]
-        y = y - offsets["y_offset"]
+        x = x - offsets["x_offset"] #Make adjustment for x-offset according to offset value read from file above
+        y = y - offsets["y_offset"] #Make adjustment for y-offset according to offset value read from file above
         
         
+        #In situations where y is zero we cannot use our trigonometry formulas, as division by zero makes the code crash
+        #In these cases heading is always either -90 or 90 depending on whether x is negative or positive
+        #Heading before making the magnetic inclination adjustment is saved as variable head
         if y == 0:
             if x < 0:
                 head = -90
             else:
                 head = 90
 
+        #In situations where y is positive, there are two scenarios to give us the type of heading we want (up to -180 for left and up to 180 for right)
         elif y > 0:
             if x > 0:
                 head = 180 - math.atan(x/y) * 180/PI
@@ -43,16 +48,14 @@ def read_compass():
         elif y < 0:
             head = - math.atan(x/y) * 180/PI
 
+        #Heading is calculated by adding the magnetic inclination in Espoo to the result from our raw trigonometry
         heading = -head + eranto #For some reason heading is reversed after calculations and hence head is here with a negative sign
         
+        #Another adjustment in case the heading is over 180, in which case we need to flip it to the range between 0 and -180
         if heading > 180:
             heading = heading - 360
-            
-#        print(heading)
-        
-#        if once == True:
-#            return(heading)
 
+        #Wait 0.1 seconds before reading the compass again
         time.sleep(0.1)
         
 if __name__ == "__main__":
